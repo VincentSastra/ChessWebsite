d00274d1291e01931335607d56c18e6cef7dddfa
VincentSastra <vincent.sastra@gmail.com>
2020-08-29 22:11:25 -0700

    Made registration page and connect the Atlas MongoDB database

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://vincent:vincent@fts.jn2zh.mongodb.net/FileTransferSystem?retryWrites=true&w=majority'
+}
--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/register', (req, res) => res.render('register'));
+
+router.post('/register', (req, res) => {
+  console.log(req.body);
+  res.send('Login Finished');
+});
+
 module.exports = router;
