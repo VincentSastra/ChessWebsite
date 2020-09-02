443045b56da2adf9d57d211565af3434558f038b
VincentSastra <vincent.sastra@gmail.com>
2020-09-02 15:05:34 -0700

    Added client react route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
+});
+
+router.get('/react', function (req, res) {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 module.exports = router;
