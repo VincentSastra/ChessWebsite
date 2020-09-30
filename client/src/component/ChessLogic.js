7922b7b6b626a4c80b790ca9d4e515da84ce2c5a
VincentSastra <vincent.sastra@gmail.com>
2020-09-30 00:31:57 -0700

    Refractor ChessLogic.js

--- client/src/component/ChessLogic.js
+++ client/src/component/ChessLogic.js
@@ -1,69 +1,9 @@
 class ChessLogic {
   constructor(tileArr = emptyArr(), castle = 'KQkq', peasant = '') {
-    // Use shallow copy
     this.Tiles = generateChessBoard(tileArr);
     this.Castle = castle;
     this.Winner = 'none';
     this.Passant = peasant;
-  }
-
-  movePiece(sourceRow, sourceCol, destRow, destCol, promotion = 'Queen') {
-    const sourceTile = this.Tiles[sourceRow][sourceCol];
-
-    let eaten = this.Tiles[destRow][destCol].getPiece();
-
-    if (eaten === 'King') {
-      this.Winner = this.Tiles[destRow][destCol].getColor() === 'white' ?
-        'Black' : 'White';
-    }
-
-    this.Tiles[destRow][destCol].piece = this.Tiles[sourceRow][sourceCol].piece;
-
-    if (sourceTile.getPiece() === 'Pawn') {
-      if ('' + destRow + destCol === this.Passant) {
-        eaten = this.Tiles[sourceRow][destCol].getPiece();
-        this.Tiles[sourceRow][destCol].piece = 'empty';
-      } else if (destRow === 0 || destRow === 7) {
-        this.Tiles[destRow][destCol].piece = sourceTile.getColor() + promotion;
-      }
-    } else if (sourceTile.getPiece() === 'King') {
-      if (Math.abs(destCol - sourceCol) > 1) {
-        if (destCol === 2) {
-          this.movePiece(sourceRow, 0, destRow, 3);
-        } else if (destCol === 6) {
-          this.movePiece(sourceRow, 7, destRow, 5);
-        }
-      }
-
-      if (sourceTile.getColor() === 'white') {
-        this.Castle = this.Castle.replace(/[KQ]/g, '');
-      } else if (sourceTile.getColor() === 'black') {
-        this.Castle = this.Castle.replace(/[kq]/g, '');
-      }
-    } else if (sourceTile.getPiece() === 'Rook') {
-      if (sourceRow === 0 && sourceCol === 0) {
-        this.Castle = this.Castle.replace('Q', '');
-      } else if (sourceRow === 0 && sourceCol === 7) {
-        this.Castle = this.Castle.replace('K', '');
-      } else if (sourceRow === 7 && sourceCol === 0) {
-        this.Castle = this.Castle.replace('q', '');
-      } else if (sourceRow === 7 && sourceCol === 7) {
-        this.Castle = this.Castle.replace('k', '');
-      }
-    }
-
-    this.Passant = '';
-
-    // Set the en passant square
-    if (sourceTile.getPiece() === 'Pawn' &&
-      Math.abs(destRow - sourceRow) === 2) {
-      const direction = sourceTile.getColor() === 'white' ? 1 : -1;
-      this.Passant = '' + (sourceRow + direction) + sourceCol;
-    }
-
-    sourceTile.piece = 'empty';
-
-    return eaten;
   }
 
   getWinner() {
@@ -74,137 +14,207 @@ class ChessLogic {
     return this.Tiles[row][col].getPiece();
   }
 
+  movePiece(sourceRow, sourceCol, destRow, destCol, promoteTo = 'Queen') {
+    const sourceTile = this.Tiles[sourceRow][sourceCol];
+    const destTile = this.Tiles[destRow][destCol];
+
+    let eaten = destTile.getPiece();
+
+    if (eaten === 'King') {
+      this.Winner = destTile.getColor() === 'white' ?
+        'Black' : 'White';
+    }
+
+    destTile.piece = sourceTile.piece;
+
+    // eslint-disable-next-line default-case
+    switch (sourceTile.getPiece()) {
+    case ('Pawn'):
+      if (this.validPassant(sourceTile, destTile)) {
+        eaten = 'Pawn';
+        this.Tiles[destRow][sourceCol].piece = 'empty';
+      }
+      this.setPassant(sourceTile, destTile);
+      this.handlePromotion(sourceTile, destTile, promoteTo);
+      break;
+    case ('King'):
+      this.handleCastle(sourceTile, destTile);
+      this.disableCastle(sourceTile);
+      break;
+    case ('Rook'):
+      this.disableCastle(sourceTile);
+      break;
+    }
+
+    sourceTile.piece = 'empty';
+
+    return eaten;
+  }
+
+  validPassant(sourceTile, destTile) {
+    return (sourceTile.getPiece() === 'Pawn' &&
+      '' + destTile.row + destTile.col === this.Passant);
+  }
+
+  setPassant(sourceTile, destTile) {
+    if (Math.abs(destTile.row - sourceTile.row) === 2) {
+      const direction = sourceTile.getColor() === 'white' ? 1 : -1;
+      this.Passant = '' + (sourceTile.row + direction) + sourceTile.col;
+    }
+  }
+
+  handlePromotion(sourceTile, destTile, promoteTo) {
+    if (destTile.row === 0 || destTile.row === 7) {
+      destTile.piece = sourceTile.getColor() + promoteTo;
+    }
+  }
+
+  handleCastle(sourceTile, destTile) {
+    if (Math.abs(destTile.col - sourceTile.col) > 1) {
+      if (destTile.col === 2) {
+        this.movePiece(sourceTile.row, 0, sourceTile.row, 3);
+      } else if (destTile.col === 6) {
+        this.movePiece(sourceTile.row, 7, sourceTile.row, 5);
+      }
+    }
+  }
+
+  disableCastle(sourceTile) {
+    let letterToReplace = sourceTile.getPiece() === 'King' ? 'KQ' :
+      sourceTile.col === 0 ? 'Q' : 'K';
+
+    if (sourceTile.getColor() === 'black') {
+      letterToReplace = letterToReplace.toLowerCase();
+    }
+
+    const regex = new RegExp(letterToReplace, '');
+    this.Castle = this.Castle.replace(regex, '');
+  }
+
   getValidTiles(row, col) {
-    const TileSet = this.Tiles;
-    const tile = TileSet[row][col];
+    const tile = this.Tiles[row][col];
 
     const color = tile.getColor();
     const piece = tile.getPiece();
 
     const validMoves = [];
 
+    // eslint-disable-next-line default-case
     switch (piece) {
-    case ('Queen'):
-
-      for (let i = 0; i < 5; i++) {
-        const colCounter = (i < 1.5) ? 1 : -1;
-        let curCol = col + colCounter;
-
-        const rowCounter = (i % 2) ? 1 : -1;
-        let curRow = row + rowCounter;
-
-        // processTile highlights the tile if the piece can go there
-        // and return true if there is a piece there
-        while (this.processTile(curRow, curCol, color, validMoves)) {
-          curCol += colCounter;
-          curRow += rowCounter;
-        }
-      }
-
-      // no breaks so it also acts as a rook
-      // eslint-disable-next-line no-fallthrough
     case ('Rook'): {
-      const rowArr = [-1, 0, 0, 1];
-      const colArr = [0, -1, 1, 0];
-
-      for (let i = 0; i < 4; i++) {
-        let curRow = rowArr[i] + row;
-        let curCol = colArr[i] + col;
-
-        // processTile highlights the tile if the piece can go there
-        // and return true if there is a piece there
-        while (this.processTile(curRow, curCol, color, validMoves)) {
-          curRow += rowArr[i];
-          curCol += colArr[i];
-        }
-      }
+      this.getValidRookTiles(col, row, color, validMoves);
       break;
     }
     case ('Knight'): {
-      for (let curRow = 1; curRow < 3; curRow++) {
-        const curCol = 3 - curRow;
-
-        // processTile highlights the tile if the piece can go there
-        // and return true if there is a piece there
-        this.processTile(row + curRow, col + curCol, color, validMoves);
-        this.processTile(row + curRow, col - curCol, color, validMoves);
-        this.processTile(row - curRow, col - curCol, color, validMoves);
-        this.processTile(row - curRow, col + curCol, color, validMoves);
-      }
+      this.getValidKnightTiles(col, row, color, validMoves);
       break;
     }
-
     case ('Bishop'): {
-      for (let i = 0; i < 5; i++) {
-        const colCounter = (i < 1.5) ? 1 : -1;
-        let curCol = col + colCounter;
-
-        const rowCounter = (i % 2) ? 1 : -1;
-        let curRow = row + rowCounter;
-
-        while (this.processTile(curRow, curCol, color, validMoves)) {
-          curCol += colCounter;
-          curRow += rowCounter;
-        }
-      }
-
+      this.getValidBishopTiles(col, row, color, validMoves);
       break;
     }
     case ('King'): {
-      for (let i = -1; i < 2; i++) {
-        for (let j = -1; j < 2; j++) {
-          this.processTile(row + i, col + j, color, validMoves);
-        }
-      }
-
-      const startRow = color === 'white' ? 0 : 7;
-
-      if (((color === 'white' && this.Castle.includes('K')) ||
-          (color === 'black' && this.Castle.includes('k'))) &&
-          TileSet[startRow][6].getColor() === 'empty' &&
-          TileSet[startRow][5].getColor() === 'empty') {
-        validMoves.push(TileSet[startRow][6]);
-      }
-
-      if (((color === 'white' && this.Castle.includes('Q')) ||
-        (color === 'black' && this.Castle.includes('q'))) &&
-        TileSet[startRow][1].getColor() === 'empty' &&
-        TileSet[startRow][2].getColor() === 'empty' &&
-        TileSet[startRow][3].getColor() === 'empty') {
-        validMoves.push(TileSet[startRow][2]);
-      }
-
+      this.getValidKingTiles(col, row, color, validMoves);
       break;
     }
-
+    case ('Queen'):
+      this.getValidBishopTiles(col, row, color, validMoves);
+      this.getValidRookTiles(col, row, color, validMoves);
+      break;
     case ('Pawn'): {
-      const distance = (row === 1 || row === 6) ? 3 : 2;
-      const direction = color === 'white' ? 1 : -1;
-
-      for (let i = 1; i < distance; i++) {
-        const curTile = TileSet[row + i * direction][col];
-
-        if (curTile.piece === 'empty') {
-          this.processTile(row + i * direction, col, color, validMoves);
-        } else {
-          break;
-        }
-      }
-
-      this.processTilePawn(row + direction, col + 1, color, validMoves);
-      this.processTilePawn(row + direction, col - 1, color, validMoves);
-
+      this.getValidPawnTiles(col, row, color, validMoves);
       break;
     }
-
-    default:
-      break;
     }
 
     return validMoves;
   }
 
-  processTile(row, col, color, validMoves) {
+  getValidKnightTiles(col, row, color, validMoves) {
+    for (let curRow = 1; curRow < 3; curRow++) {
+      const curCol = 3 - curRow;
+
+      this.pushToValid(row + curRow, col + curCol, color, validMoves);
+      this.pushToValid(row + curRow, col - curCol, color, validMoves);
+      this.pushToValid(row - curRow, col - curCol, color, validMoves);
+      this.pushToValid(row - curRow, col + curCol, color, validMoves);
+    }
+  }
+
+  getValidRookTiles(col, row, color, validMoves) {
+    const rowArr = [-1, 0, 0, 1];
+    const colArr = [0, -1, 1, 0];
+
+    for (let i = 0; i < 4; i++) {
+      let curRow = rowArr[i] + row;
+      let curCol = colArr[i] + col;
+
+      while (this.pushToValid(curRow, curCol, color, validMoves)) {
+        curRow += rowArr[i];
+        curCol += colArr[i];
+      }
+    }
+  }
+
+  getValidBishopTiles(col, row, color, validMoves) {
+    for (let i = 0; i < 5; i++) {
+      const colCounter = (i < 1.5) ? 1 : -1;
+      let curCol = col + colCounter;
+
+      const rowCounter = (i % 2) ? 1 : -1;
+      let curRow = row + rowCounter;
+
+      while (this.pushToValid(curRow, curCol, color, validMoves)) {
+        curCol += colCounter;
+        curRow += rowCounter;
+      }
+    }
+  }
+
+  getValidKingTiles(col, row, color, validMoves) {
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        this.pushToValid(row + i, col + j, color, validMoves);
+      }
+    }
+
+    const startRow = color === 'white' ? 0 : 7;
+
+    if (((color === 'white' && this.Castle.includes('K')) ||
+      (color === 'black' && this.Castle.includes('k'))) &&
+      this.Tiles[startRow][6].getColor() === 'empty' &&
+      this.Tiles[startRow][5].getColor() === 'empty') {
+      validMoves.push(this.Tiles[startRow][6]);
+    }
+
+    if (((color === 'white' && this.Castle.includes('Q')) ||
+      (color === 'black' && this.Castle.includes('q'))) &&
+      this.Tiles[startRow][1].getColor() === 'empty' &&
+      this.Tiles[startRow][2].getColor() === 'empty' &&
+      this.Tiles[startRow][3].getColor() === 'empty') {
+      validMoves.push(this.Tiles[startRow][2]);
+    }
+  }
+
+  getValidPawnTiles(col, row, color, validMoves) {
+    const distance = (row === 1 || row === 6) ? 3 : 2;
+    const direction = color === 'white' ? 1 : -1;
+
+    for (let i = 1; i < distance; i++) {
+      const curTile = this.Tiles[row + i * direction][col];
+
+      if (curTile.piece === 'empty') {
+        this.pushToValid(row + i * direction, col, color, validMoves);
+      } else {
+        break;
+      }
+    }
+
+    this.pushToValidPawn(row + direction, col + 1, color, validMoves);
+    this.pushToValidPawn(row + direction, col - 1, color, validMoves);
+  }
+
+  pushToValid(row, col, color, validMoves) {
     if (row > 7 || row < 0 || col > 7 || col < 0) {
       return false;
     }
@@ -221,7 +231,7 @@ class ChessLogic {
     }
   }
 
-  processTilePawn(row, col, color, validMoves) {
+  pushToValidPawn(row, col, color, validMoves) {
     if (row > -1 && row < 8 && col > -1 && col < 8) {
       const tile = this.Tiles[row][col];
 
