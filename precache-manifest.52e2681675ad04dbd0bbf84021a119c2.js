e1d315a93a1a9c74a5da0534ebafc2f6024262b9
VincentSastra <vincent.sastra@gmail.com>
2020-10-08 02:17:56 -0700

    Updates

---
+++ precache-manifest.52e2681675ad04dbd0bbf84021a119c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72cc2089c06fa326858568a5aab6dd16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b0847aae7017b50afed",
+    "url": "/static/css/2.98a65e54.chunk.css"
+  },
+  {
+    "revision": "0f40d8fafe61ed113743",
+    "url": "/static/css/main.5a1348b9.chunk.css"
+  },
+  {
+    "revision": "4b0847aae7017b50afed",
+    "url": "/static/js/2.5d870f51.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5d870f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f40d8fafe61ed113743",
+    "url": "/static/js/main.4356eec3.chunk.js"
+  },
+  {
+    "revision": "0eef5959ec0785243101",
+    "url": "/static/js/runtime-main.4e7430f6.js"
+  }
+]);
