acdaa2daecee08b9da4c3a55e59b183c5982bf8a
VincentSastra <vincent.sastra@gmail.com>
2020-10-29 13:49:21 -0700

    Fix build bugs

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,7 +8,7 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/react', function(req, res) {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
 });
 
 module.exports = router;
